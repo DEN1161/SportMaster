7c38345964e11ab6eac969e7b2b001dbcafb5e46
DEN1161 <49274462+DEN1161@users.noreply.github.com>
2019-09-20 22:08:41 +0300

    Update SportMaster.java

--- src/SportMaster.java
+++ src/SportMaster.java
@@ -1,33 +1,30 @@
 public class SportMaster {
     public static void main(String[] args) {
 
-        int PurchaseAmount = 14_000;
-        int CurrentAmount = 3000;
-        int bonus;
-        int AmountForBonus = 1000;
+        int purchaseAmount = 14_000;
+        int currentAmount = 3000;
+        int amountForBonus = 1000;
+        
+        int standartCard = 15_000;
+        int standartBonus = 50;
 
-        // Виды бонусных карт и бонусов
-        int Standart_Card = 15_000;
-        int Standart_Bonus = 50;
+        int silverCard = 15_0000;
+        int silverBonus = 70;
 
-        int Silver_Card = 15_0000;
-        int Silver_Bonus = 70;
+        int goldCard = 15_0000;
+        int goldBonus = 100;
+       
+        int bonus = 0;
+        int bonusMultiplication = purchaseAmount / amountForBonus;
+        int amountAllPurchase = purchaseAmount + currentAmount;
 
-        int Gold_Card = 15_0000;
-        int Gold_Bonus = 100;
-
-        int AmountAllPurchase = PurchaseAmount + CurrentAmount;
-
-        if(AmountAllPurchase <= Standart_Card){
-            bonus = CurrentAmount / AmountForBonus * Standart_Bonus;
-            System.out.println(bonus);
-        }else if(AmountAllPurchase <= Silver_Card){
-            bonus = CurrentAmount / AmountForBonus * Silver_Bonus;
-            System.out.println(bonus);
-        }else if(AmountAllPurchase > Gold_Card){
-            bonus = CurrentAmount / AmountForBonus* Gold_Bonus;
-            System.out.println(bonus);
+        if(amountAllPurchase <= standartCard){
+            bonus = bonusMultiplication * standartBonus;
+        }else if(amountAllPurchase <= silverCard){
+             bonus = bonusMultiplication * silverBonus;
+        }else if(amountAllPurchase > goldCard){
+             bonus = bonusMultiplication * goldBonus;
         }
-
+        System.out.println(bonus);
     }
 }
